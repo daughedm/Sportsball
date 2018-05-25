cc546d1e7e178d168ec5f4cac7392fedae9be90c
David Daugherty <david.m.daugherty@gmail.com>
2018-05-25 10:59:39 -0600

    Added folder structure and apiKey

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,19 +1,11 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+import Navigation from './Navigation';
+
+const App = () =>
+  < Router >
+  < LoginPage />
+  </Router>
 
 export default App;
