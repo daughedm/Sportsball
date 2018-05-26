529abd12150eec6147ea1a7751b8afdb949d7663
David Daugherty <david.m.daugherty@gmail.com>
2018-05-26 17:44:35 -0600

    Add rootReducer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
+import { BrowserRouter as Router } from 'react-router-dom';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from '../src/reducers/index';
+import App from './containers/App/App';
 import './index.css';
-import App from './Containers/App/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(rootReducer, devTools);
+
+// store.subscribe(() => {
+//   saveState(store.getState());
+// });
+
+render( 
+  <Provider store={ store }>
+    <Router >
+      <App />
+    </Router> 
+  </Provider>,
+  document.getElementById('root')
+);
