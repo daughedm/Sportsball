d2539a04851a6dee04c0529dc3b8e0372a376f0a
David Daugherty <david.m.daugherty@gmail.com>
2018-06-04 19:29:39 -0600

    Add button toggle

---
+++ src/Containers/TeamScroll/TeamScroll.js
@@ -0,0 +1,92 @@
+import React, {Component} from 'react'
+import mlbTeams from './../../Helpers/mlbTeams';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { findGame, teamStatCleaner } from '../../Helpers/dataCleaner'
+import { addSelectedTeamToStore } from '../../Actions/userActions';
+import { addSingleSummaryToStore, addSingleBoxScoreToStore, addTeamStatsToStore } from '../../Actions';
+import { teamStatsFetch } from '../../ApiCall/ApiCall';
+
+
+class TeamScroll extends Component {
+  constructor() {
+    super();
+    this.state = {
+      teamClicked: ''
+    };
+  }
+
+  handleTeamClick = (event) => {
+    this.setState({
+      teamClicked: event.target.id
+    })
+  }
+
+  handleTeamSubmit = async (event) => {
+    event.preventDefault();
+    const allTeamStats = await teamStatsFetch(this.state.teamClicked);
+    const teamStats = teamStatCleaner(allTeamStats)
+    this.props.handleTeamStats(teamStats);
+    const singleBoxScore = findGame(this.state.teamClicked, this.props.boxScores);
+    const singleSummary = findGame(this.state.teamClicked, this.props.gameSummaries);
+
+    this.props.handleTeamSelect(this.state.teamClicked);
+    this.props.handleBoxScore(singleBoxScore);
+    this.props.handleGameSummary(singleSummary);
+  }
+
+  render() {
+    console.log(mlbTeams)
+    const teamList = mlbTeams.map(team => {
+      const toggleClass = this.state.teamClicked === team.id ?  "selected-team" :  ''; 
+      
+      return (
+        <li 
+          className={`${toggleClass} list-item`} 
+          onClick={ this.handleTeamClick } 
+          id={team.id}> 
+          {team.teamName}
+        </li>)
+    })
+    return(
+        <div>
+          <ul 
+            className="unordered-list dropdown-menu scrollable-menu"
+            role="menu">
+            {teamList}
+          </ul>
+        <button
+          onClick={this.handleTeamSubmit} 
+          className="submit-button"
+          type="submit"
+        >
+          select team
+        </button>
+        </div>
+    )
+
+  }  
+}
+
+export const mapDispatchToProps = (dispatch) => ({
+  handleTeamSelect: (teamClicked) =>
+    dispatch(addSelectedTeamToStore(teamClicked)),
+  handleGameSummary: (summary) =>
+    dispatch(addSingleSummaryToStore(summary)),
+  handleBoxScore: (boxscore) =>
+    dispatch(addSingleBoxScoreToStore(boxscore)),
+  handleTeamStats: (teamStats) =>
+    dispatch(addTeamStatsToStore(teamStats))
+});
+
+export const mapStateToProps = (state) => ({
+  selectedTeam: state.selectedTeam,
+  gameSummaries: state.gameSummaries,
+  boxScores: state.boxScores
+})
+
+TeamScroll.propTypes = {
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TeamScroll)
--- src/Containers/TeamSelect/TeamSelect.js
+++ src/Containers/TeamSelect/TeamSelect.js
@@ -6,6 +6,7 @@ import { addSelectedTeamToStore } from '../../Actions/userActions';
 import './TeamSelect.css';
 import { addSingleSummaryToStore, addSingleBoxScoreToStore, addTeamStatsToStore } from '../../Actions';
 import {teamStatsFetch} from '../../ApiCall/ApiCall';
+import TeamScroll from './../TeamScroll/TeamScroll';
 
 export class TeamSelect extends Component {
   constructor(props) {
@@ -40,54 +41,17 @@ export class TeamSelect extends Component {
       <form
         className = "login-form"
         type = "submit"
-        onSubmit={this.handleTeamSubmit} 
+        // onSubmit={this.handleTeamSubmit} 
       >
         <h3 className = "form-header" > Select Your Team </h3> 
-           < ul className = "unordered-list dropdown-menu scrollable-menu"
-           role = "menu" >
-            <li className="list-item" onClick={this.handleTeamClick} id={"Diamondbacks"}>Arizona Diamondbacks</li>
-            <li className="list-item" onClick={this.handleTeamClick} id="Braves">Atlanta Braves</li>
-            <li className="list-item" onClick={this.handleTeamClick} id="Orioles">Baltimore Orioles</li>
-            <li className="list-item" onClick={this.handleTeamClick} id="Red Sox">Boston Red Sox</li>
-            <li className="list-item" onClick={this.handleTeamClick} id="Cubs">Chicago Cubs</li>
-            <li className="list-item" onClick={this.handleTeamClick} id="White Sox">Chicago White Sox</li>
-            <li className="list-item" onClick={this.handleTeamClick} id="Reds">Cincinnati Reds</li>
-            <li className="list-item" onClick={this.handleTeamClick} id="Indians">Cleveland Indians</li>
-            <li className="list-item" onClick={this.handleTeamClick} id="Rockies">Colorado Rockies</li>
-            <li className="list-item" onClick={this.handleTeamClick} id="Tigers">Detroit Tigers</li>
-            <li className="list-item" onClick={this.handleTeamClick} id="Astros">Houston Astros</li>
-            <li className="list-item" onClick={this.handleTeamClick} id="Royals">Kansas City Royals</li>
-            <li className="list-item" onClick={this.handleTeamClick} id="Angels">LA Angels of Anaheim</li>
-            <li className="list-item" onClick={this.handleTeamClick} id="Dodgers">Los Angeles Dodgers</li>
-            <li className="list-item" onClick={this.handleTeamClick} id="Marlins">Miami Marlins</li>
-            <li className="list-item" onClick={this.handleTeamClick} id="Brewers">Milwaukee Brewers</li>
-            <li className="list-item" onClick={this.handleTeamClick} id="Twins">Minnesota Twins</li>
-            <li className="list-item" onClick={this.handleTeamClick} id="Mets">New York Mets</li>
-            <li className="list-item" onClick={this.handleTeamClick} id="Yankees">New York Yankees</li>
-            <li className="list-item" onClick={this.handleTeamClick} id="Athletics">Oakland Athletics</li>
-            <li className="list-item" onClick={this.handleTeamClick} id="Phillies">Philadelphia Phillies</li>
-            <li className="list-item" onClick={this.handleTeamClick} id="Pirates">Pittsburgh Pirates</li>
-            <li className="list-item" onClick={this.handleTeamClick} id="Padres">San Diego Padres</li>
-            <li className="list-item" onClick={this.handleTeamClick} id="Giants">San Francisco Giants</li>
-            <li className="list-item" onClick={this.handleTeamClick} id="Mariners">Seattle Mariners</li>
-            <li className="list-item" onClick={this.handleTeamClick} id="Cardinals">St. Louis Cardinals</li>
-            <li className="list-item" onClick={this.handleTeamClick} id="Rays">Tampa Bay Rays</li>
-            <li className="list-item" onClick={this.handleTeamClick} id="Rangers">Texas Rangers</li>
-            <li className="list-item" onClick={this.handleTeamClick} id="Blue Jays">Toronto Blue Jays</li>
-            <li className="list-item" onClick={this.handleTeamClick} id="Nationals">Washington Nationals</li>
-          </ul>
-        <button
-          className = "submit-button"
-          type = "submit" 
-        >
-         select team 
-        </button>
+           <TeamScroll />
+     
       </form>
     )
   }
 }
 
-const mapStateToProps = (state) => ({
+export const mapStateToProps = (state) => ({
   selectedTeam: state.selectedTeam,
   gameSummaries: state.gameSummaries,
   boxScores: state.boxScores
@@ -108,4 +72,8 @@ TeamSelect.propTypes = {
 
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(TeamSelect)
+
+const mapAllTheProps = connect(mapStateToProps, mapDispatchToProps);
+export default mapAllTheProps(TeamSelect)
+
+// export default teamData(mlbData(userData(TeamSelect)))
